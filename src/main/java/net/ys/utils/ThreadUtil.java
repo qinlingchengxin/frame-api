0d6e83a0899034f8856c6e8fe7a4e3dd32055cb9
qinlingchengxin <2991821761@qq.com>
2018-12-17 14:23:29 +0800

    update

---
+++ src/main/java/net/ys/utils/ThreadUtil.java
@@ -0,0 +1,20 @@
+package net.ys.utils;
+
+import java.util.concurrent.TimeUnit;
+
+public class ThreadUtil {
+
+    public static void sleep(long millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+        }
+    }
+
+    public static void sleep(long duration, TimeUnit unit) {
+        try {
+            Thread.sleep(unit.toMillis(duration));
+        } catch (InterruptedException e) {
+        }
+    }
+}
