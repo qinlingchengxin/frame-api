81c701dff6fcc1e2908e1a1302a43bc30feb1cd7
qinlingchengxin <2991821761@qq.com>
2018-12-19 19:22:38 +0800

    update

--- src/main/java/net/ys/component/SysConfig.java
+++ src/main/java/net/ys/component/SysConfig.java
@@ -1,7 +1,10 @@
 package net.ys.component;
 
+import net.ys.utils.PropertyUtil;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 /**
  * 系统配置类
@@ -13,8 +16,18 @@ public class SysConfig {
 
     public static String appName;
 
+    public static String[] testName;
+
     @Value("${app.name}")
     public void setAppName(String appName) {
         this.appName = appName;
+    }
+
+    @Value("${testName:test.name.*}")
+    public void setTestName(String[] testName) {
+        String prefix = testName[0];
+        List<String> nameList = PropertyUtil.gets(prefix.substring(0, prefix.length() - 1));
+        String[] names = new String[nameList.size()];
+        this.testName = nameList.toArray(names);
     }
 }
