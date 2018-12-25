d917d586f81072528e1044c718e4d401c2c7c4c3
qinlingchengxin <2991821761@qq.com>
2018-12-25 13:19:18 +0800

    update

--- src/main/java/net/ys/constant/X.java
+++ src/main/java/net/ys/constant/X.java
@@ -9,9 +9,6 @@ public interface X {
     String SEPARATOR = "/";
 
     interface Time {
-        int DAY_SECOND = 24 * 60 * 60;
-        int HOUR_SECOND = 60 * 60;
-        int MINUTE = 60;
         int DAY_MILLISECOND = 24 * 60 * 60 * 1000;
         int HOUR_MILLISECOND = 60 * 60 * 1000;
         int MINUTE_MILLISECOND = 60 * 1000;
