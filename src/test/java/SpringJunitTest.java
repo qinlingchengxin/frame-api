8f119e6e2b227728acafcc34cdad00c5f974e32c
qinlingchengxin <34084847+qinlingchengxin@users.noreply.github.com>
2019-04-07 17:34:06 +0800

    spring测试类
    spring测试类

---
+++ src/test/java/SpringJunitTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * User: NMY
+ * Date: 19-4-7
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:applicationContext.xml"})
+public class SpringJunitTest {
+
+    @Test
+    public void test() {
+        System.out.println("test");
+    }
+}
