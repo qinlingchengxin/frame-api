8993fa20a0ce180216b88e163c26eb511a1b6a3f
qinlingchengxin <2991821761@qq.com>
2019-05-08 13:02:50 +0800

    update

--- src/main/java/net/ys/service/DemoService.java
+++ src/main/java/net/ys/service/DemoService.java
@@ -4,15 +4,34 @@ import net.ys.constant.MsgKey;
 import net.ys.threadpool.ThreadPoolManager;
 import net.ys.utils.LogUtil;
 import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.filefilter.IOFileFilter;
 import org.apache.commons.io.filefilter.TrueFileFilter;
+import org.apache.http.StatusLine;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContextBuilder;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.security.GeneralSecurityException;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
 import java.util.Collection;
 
 @Service
@@ -58,6 +77,64 @@ public class DemoService {
         } catch (Exception e) {
             LogUtil.error(e);
         }
+    }
+
+    public static String httpsPost(String url) {
+        RequestConfig requestConfig = RequestConfig.custom().setSocketTimeout(60000).setConnectTimeout(60000).setConnectionRequestTimeout(60000).build();
+        CloseableHttpClient httpClient = null;
+        CloseableHttpResponse response = null;
+        try {
+            httpClient = HttpClients.custom().setSSLSocketFactory(createSSLConnSocketFactory()).setDefaultRequestConfig(requestConfig).build();
+            HttpPost httpPost = new HttpPost(url);
+            response = httpClient.execute(httpPost);
+            StatusLine status = response.getStatusLine();
+            int state = status.getStatusCode();
+            if (state == HttpStatus.SC_OK) {
+                StringBuffer sb = new StringBuffer("");
+                BufferedReader in = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "utf-8"));
+                String line;
+                String separator = System.getProperty("line.separator");
+                while ((line = in.readLine()) != null) {
+                    sb.append(line + separator);
+                }
+                in.close();
+
+                return sb.toString();
+            } else {
+                System.out.println("状态码：" + state);
+            }
+        } catch (Exception e) {
+            LogUtil.error(e);
+        } finally {
+            try {
+                if (response != null) {
+                    response.close();
+                }
+                if (httpClient != null) {
+                    httpClient.close();
+                }
+            } catch (IOException e) {
+            }
+        }
+        return "";
+    }
+
+    private static SSLConnectionSocketFactory createSSLConnSocketFactory() {
+        try {
+            SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
+                public boolean isTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+                    return true;
+                }
+            }).build();
+            return new SSLConnectionSocketFactory(sslContext, new HostnameVerifier() {
+                @Override
+                public boolean verify(String s, SSLSession sslSession) {
+                    return true;
+                }
+            });
+        } catch (GeneralSecurityException e) {
+        }
+        return null;
     }
 
     public void testFile() {
