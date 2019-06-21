c00ca2da88c47c79ad67a196c7cc4b04fec7349a
qinlingchengxin <2991821761@qq.com>
2019-06-21 19:09:59 +0800

    update

--- src/main/java/net/ys/util/PropertyUtil.java
+++ src/main/java/net/ys/util/PropertyUtil.java
@@ -1,5 +1,7 @@
 package net.ys.util;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
@@ -27,6 +29,30 @@ public class PropertyUtil {
     }
 
     public static List<String> gets(String prefixKey) {
+        List<String> values = new ArrayList<String>();
+        Enumeration<?> names = properties.propertyNames();
+        String name;
+        while (names.hasMoreElements()) {
+            name = String.valueOf(names.nextElement());
+            if (name.startsWith(prefixKey)) {
+                values.add(properties.getProperty(name));
+            }
+        }
+        return values;
+    }
+
+    public static String reGet(String key) throws IOException {
+        Properties properties = new Properties();
+        String path = PropertyUtil.class.getClassLoader().getResource("config.properties").getPath();
+        properties.load(new FileInputStream(path));
+        return properties.getProperty(key);
+    }
+
+    public static List<String> reGets(String prefixKey) throws IOException {
+        Properties properties = new Properties();
+        String path = PropertyUtil.class.getClassLoader().getResource("config.properties").getPath();
+        properties.load(new FileInputStream(path));
+
         List<String> values = new ArrayList<String>();
         Enumeration<?> names = properties.propertyNames();
         String name;
