2bab858f219002972c703e1b0db06aa0187e8b37
mrlyxxyl <529726271@qq.com>
2019-09-11 17:41:09 +0800

    update encrypt method

--- src/main/java/net/ys/listener/SystemListener.java
+++ src/main/java/net/ys/listener/SystemListener.java
@@ -1,11 +1,15 @@
 package net.ys.listener;
 
+import com.mysql.jdbc.AbandonedConnectionCleanupThread;
 import net.ys.threadpool.ThreadPoolManager;
 import net.ys.util.Tools;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
 
 /**
  * User: NMY
@@ -17,6 +21,15 @@ public class SystemListener implements ServletContextListener {
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
         ThreadPoolManager.INSTANCE.destroy();
+        try {//释放DB驱动并关闭线程
+            Enumeration<Driver> enumeration = DriverManager.getDrivers();
+            while (enumeration.hasMoreElements()) {
+                DriverManager.deregisterDriver(enumeration.nextElement());
+            }
+            AbandonedConnectionCleanupThread.shutdown();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
         System.out.println("contextDestroyed");
     }
 
