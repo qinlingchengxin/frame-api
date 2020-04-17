3ad2045a83895ee2cb7ffb61a58d78ff71ee766f
mrlyxxyl <529726271@qq.com>
2020-04-17 18:38:29 +0800

    修改项目的启动方式：-Dspring.profiles.active=prod

---
+++ src/main/java/net/ys/component/SystemAspect.java
@@ -0,0 +1,23 @@
+package net.ys.component;
+
+import net.ys.util.LogUtil;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class SystemAspect {
+
+    @Around(value = "execution(* net.ys.controller..*.*(..))", argNames = "pjp")
+    public Object validator(ProceedingJoinPoint pjp) throws Throwable {
+        Object[] args = pjp.getArgs();
+        String methodName = pjp.getTarget().getClass().getSimpleName() + "." + pjp.getSignature().getName();
+        long start = System.currentTimeMillis();
+        Object obj = pjp.proceed(args);
+        long useTime = System.currentTimeMillis() - start;
+        LogUtil.debug("SystemAspect================================>method:[" + methodName + "]\t use time:[" + useTime + "ms]");
+        return obj;
+    }
+}
