97bc71571534983fe9ee0bc550e4c65a4924ebb7
mrlyxxyl <529726271@qq.com>
2020-04-20 08:15:31 +0800

    修改项目的启动方式：-Dspring.profiles.active=prod

--- src/main/java/net/ys/component/SystemAspect.java
+++ src/main/java/net/ys/component/SystemAspect.java
@@ -14,10 +14,18 @@ public class SystemAspect {
     public Object validator(ProceedingJoinPoint pjp) throws Throwable {
         Object[] args = pjp.getArgs();
         String methodName = pjp.getTarget().getClass().getSimpleName() + "." + pjp.getSignature().getName();
+
+        String newLine = "\r\n";
+        StringBuffer sb = new StringBuffer("SystemAspect------>method:[" + methodName + "]" + newLine);
+        for (int i = 0; i < args.length; i++) {
+            sb.append(i + "------>" + args[i] + newLine);
+        }
+        LogUtil.debug(sb.toString());
+
         long start = System.currentTimeMillis();
         Object obj = pjp.proceed(args);
         long useTime = System.currentTimeMillis() - start;
-        LogUtil.debug("SystemAspect================================>method:[" + methodName + "]\t use time:[" + useTime + "ms]");
+        LogUtil.debug("SystemAspect------>method:[" + methodName + "]\t use time:[" + useTime + "ms]");
         return obj;
     }
 }
